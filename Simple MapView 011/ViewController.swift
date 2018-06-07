9a4bd736f4d18e25af6d5c2009d7cc12694e0c5c
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-06-07 11:56:39 +0900

    ere

--- Simple MapView 011/ViewController.swift
+++ Simple MapView 011/ViewController.swift
@@ -7,17 +7,28 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        //위도, 경도 설정
+    
+        let loc : CLLocationCoordinate2D =
+            CLLocationCoordinate2D(latitude:35.166025, longitude: 129.070354)
+        let span = MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)
+        let region = MKCoordinateRegion(center: loc, span: span)
+        myMapView.setRegion(region, animated: true)
+        
+        let anno = MKPointAnnotation()
+        anno.coordinate = loc
+        anno.title = "DIT 동의과학대학교"
+        anno.subtitle = "내 꿈이 자라는 곳"
+        myMapView.addAnnotation(anno)
+    
     }
 
 
